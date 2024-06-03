0e8bafb085d893c91f4e6087e3743c0ee9d0effe
Bl3nd865 <blend865r@gmail.com>
2024-06-03 17:45:08 +0200

    pulling

---
+++ BBShop/Migrations/20240530103041_intitial.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BBShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class intitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_AspNetUsers_BuyerId",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Orders",
+                newName: "OrderDate");
+
+            migrationBuilder.RenameColumn(
+                name: "BuyerId",
+                table: "Orders",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Orders_BuyerId",
+                table: "Orders",
+                newName: "IX_Orders_UserId");
+
+            migrationBuilder.RenameColumn(
+                name: "UnitPrice",
+                table: "OrderItems",
+                newName: "Price");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Products",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "StoreId",
+                table: "Orders",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalAmount",
+                table: "Orders",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_UserId",
+                table: "Products",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_StoreId",
+                table: "Orders",
+                column: "StoreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_AspNetUsers_UserId",
+                table: "Orders",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Stores_StoreId",
+                table: "Orders",
+                column: "StoreId",
+                principalTable: "Stores",
+                principalColumn: "StoreId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_AspNetUsers_UserId",
+                table: "Products",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_AspNetUsers_UserId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Stores_StoreId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_AspNetUsers_UserId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_UserId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_StoreId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "StoreId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "TotalAmount",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Orders",
+                newName: "BuyerId");
+
+            migrationBuilder.RenameColumn(
+                name: "OrderDate",
+                table: "Orders",
+                newName: "CreatedAt");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders",
+                newName: "IX_Orders_BuyerId");
+
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "OrderItems",
+                newName: "UnitPrice");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_AspNetUsers_BuyerId",
+                table: "Orders",
+                column: "BuyerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
